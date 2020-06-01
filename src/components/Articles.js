3179318f600655d682471a9af488129841f9553e
Tom Klaver <tom@codecapi.nl>
2020-06-01 22:29:08 +0200

    use slugs i/o IDs

--- src/components/Articles.js
+++ src/components/Articles.js
@@ -31,35 +31,31 @@ export const Articles = () => {
   `);
 
   const col1 = [
-    "4c77f9cb-95f1-5bb2-b0d3-dacb748ca55d",
-
-    "f7d39213-ec04-5b8e-9064-2901d894155b",
+    "/article/anderhalve-meter/",
+    "/article/2020-06-01-de-titel-2/",
   ];
 
   const col2 = [
-    "8b3e811f-c7ca-506a-bdcd-6f7147f919a7",
-
-    "7e217aac-93c5-53ac-80b1-ad5af1789499",
+    "/article/lilly-hamsterde-aan-het-begin-van-de-coronacrisis-zoveel-mogelijk-vitamine-c-bij-elkaar/",
+    "/article/2020-06-01-de-titel-2-1/",
   ];
 
   const col3 = [
-    "10d9a152-9d14-589f-a1bf-670982382fe8",
-
-    "1b2c40f0-c3db-5346-bf67-600fc577e71c",
+    "/article/2020-06-01-de-titel-3/",
+    "/article/2020-06-01-de-titel/",
   ];
 
   const nodes = data.allMarkdownRemark.edges.map((edge) => edge.node);
-
   console.log(
     "article IDs not in column: ",
     nodes
-      .map((node) => node.id)
-      .filter((id) => !col1.concat(col2).concat(col3).includes(id))
+      .map((node) => node.fields.slug)
+      .filter((slug) => !col1.concat(col2).concat(col3).includes(slug))
   );
 
-  const col1items = nodes.filter((node) => col1.includes(node.id));
-  const col2items = nodes.filter((node) => col2.includes(node.id));
-  const col3items = nodes.filter((node) => col3.includes(node.id));
+  const col1items = nodes.filter((node) => col1.includes(node.fields.slug));
+  const col2items = nodes.filter((node) => col2.includes(node.fields.slug));
+  const col3items = nodes.filter((node) => col3.includes(node.fields.slug));
 
   return (
     <>
